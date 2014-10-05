df3ae2c062789d3f7e945e321099e1081668018d
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-05 23:18:44 +0200

    login support

--- didel/session.py
+++ didel/session.py
@@ -1,15 +1,21 @@
 # -*- coding: UTF-8 -*-
 
+from bs4 import BeautifulSoup
 from cookielib import LWPCookieJar
 from os.path import expanduser, isfile
 from requests import Session as BaseSession
 
 COOKIES_FILE = expanduser('~/.didel.cookies')
 
+URLS = {
+    'login': 'https://auth.univ-paris-diderot.fr/cas/login',
+    'logout': 'https://auth.univ-paris-diderot.fr/cas/logout',
+}
 
 class Session(BaseSession):
     """
-    A session with persistent cookies storage
+    A session with persistent cookies storage and builtin authentification
+    support for Paris Diderot's websites.
     """
 
     def __init__(self, *args, **kwargs):
@@ -37,6 +43,27 @@ class Session(BaseSession):
         """
         if isfile(self.cookies_file):
             self.cookies.load()
+
+    def login(self, username, passwd):
+        """
+        Authenticate an user
+        """
+        url = URLS['login']
+        soup = BeautifulSoup(self.get(url).text)
+        lts = soup.select('input[name=lt]')
+        if not lts:
+            return False
+        formkey = lts[0].attrs['value']
+        resp = self.post(url, {
+            'username': username,
+            'password': passwd,
+            'lt': formkey,
+            '_eventId': 'submit',
+        })
+        return resp.ok and 'Log In Successful' in resp.text
+
+    def logout(self):
+        return 'Logout successful' in self.get(URLS['logout']).text
 
     def __del__(self):
         self.save()
