a2f02d5c7543c9363d049dc3ae71abbbc71c4aa2
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-07 23:20:36 +0200

    CLI config:* subcommands

--- didel/cli.py
+++ didel/cli.py
@@ -5,12 +5,29 @@ from __future__ import print_function
 from sys import argv
 
 from didel import __version__
+from didel.config import DidelConfig
+
+HELP_FLAGS = ('-h', '-help', '--help')
+
+
+def get_args(fun):
+    """
+    Get a list of the arguments of a function
+    """
+    # http://stackoverflow.com/a/4051447/735926
+    try:
+        code = fun.func_code
+    except AttributeError:  # Python 3
+        code = fun.__code__
+    return code.co_varnames[:code.co_argcount]
+
 
 class DidelCli(object):
 
     def __init__(self, argv):
         self.argv = argv
         self.exe = self.argv.pop(0)
+        self.config = DidelConfig.get_default()
 
 
     def print_version(self):
@@ -19,8 +36,7 @@ class DidelCli(object):
 
     def print_help(self):
         name_offset = len('action_')
-        self.print_version()
-        print("\nUsage:\n\tdidel <subcommand> args...")
+        print("\nUsage:\n\t%s <subcommand> args..." % self.exe)
         print("\nAvailable subcommands:")
         for mth in dir(self):
             if not mth.startswith('action_'):
@@ -31,21 +47,72 @@ class DidelCli(object):
             print("%10s -- %65s" % (name, doc))
 
 
+    def print_action_help(self, action, params, docstring=''):
+        """
+        Print an help text for a subcommand
+        """
+        params = map(lambda s: '<%s>' % s, params)
+        print("Usage:\n\t%s %s %s\n" % (self.exe, action, " ".join(params)))
+        if docstring:
+            print("%s\n" % docstring.strip())
+
+
+    def action_config_set(self, key, value):
+        """
+        Set a configuration variable
+        """
+        self.config.set(key, value)
+
+
+    def action_config_get(self, key):
+        """
+        Get a configuration variable
+        """
+        print(self.config.get(key))
+
+
+    def action_config_list(self):
+        """
+        Print the actual configuration
+        """
+        for kv in self.config.items():
+            print('%s=%s' % kv)
+
+
     def run(self):
+        """
+        Parse the command-line arguments and call the method corresponding to
+        the situation.
+        """
+        # We're using a custom parser here to handle subcommands.
         if len(self.argv) == 0:
             return self.print_help()
         action = self.argv.pop(0)
-        if action in ('-h', '-help', '--help'):
+        if action in HELP_FLAGS:
+            self.print_version()
             return self.print_help()
         if action in ('-v', '-version', '--version'):
             return self.print_version()
 
+        # Subcommands are defined as below:
+        #   def action_some_keyword(self, ...)
+        # defines an action 'some:keyword'. We might need to use classes for
+        # subcommands like Thor (Ruby gem), but it'd be too much overhead for
+        # now.
         name = 'action_%s' % action.replace(':', '_')
         if not hasattr(self, name):
             print("Unrecognized action '%s'" % action)
             return self.print_help()
 
-        return getattr(self, name)(self.argv)
+        mth = getattr(self, name)
+        argv = self.argv
+        argc = len(argv)
+        params = get_args(mth)[1:]  # remove 'self'
+
+        if argc != len(params) or (argc > 0 and argv[0] in HELP_FLAGS):
+            return self.print_action_help(action, params, mth.__doc__)
+
+        return mth(*argv)
 
 
 
---
+++ didel/config.py
@@ -0,0 +1,101 @@
+# -*- coding: UTF-8 -*-
+
+try:
+    from ConfigParser import SafeConfigParser
+except ImportError:  # Python 3
+    from configparser import SafeConfigParser
+
+from os import chmod
+from os.path import expanduser, isfile
+
+
+class DidelConfig(object):
+    """
+    A wrapper for Python's ConfigParser which uses paths instead of sections
+
+    >>> config = DidelConfig("foo.conf")
+    >>> config.set("foo", 45)
+    >>> config.get("foo")
+    "45"
+    >>> config.set("stuff.bar", "hello")
+    >>> config.get("stuff.bor")
+    None
+    >>> config.get("stuff.bar")
+    "hello"
+    """
+
+    SOURCE_FILE = expanduser('~/.didel.conf')
+    _default = None
+
+    @classmethod
+    def get_default(cls):
+        """
+        Get the default config object.
+        """
+        if not cls._default:
+            cls._default = cls()
+        return cls._default
+
+
+    def __init__(self, filename=SOURCE_FILE):
+        self.filename = filename
+        self.config = SafeConfigParser()
+        if not isfile(self.filename):
+            self.save()
+        self.load()
+
+
+    def load(self):
+        self.config.read(self.filename)
+
+
+    def save(self):
+        with open(self.filename, 'w') as f:
+            self.config.write(f)
+        chmod(self.filename, 0600)
+
+
+    def _split_path(self, key):
+        """
+        Split a key into a ``(section, key)`` tuple.
+        """
+        return key.split('.', 1) if '.' in key else ('DEFAULT', key)
+
+
+    def set(self, key, value):
+        """
+        Set a value. It creates the section if it doesn't exist
+
+        >>> config.set("foo.bar", 42)
+        """
+        section, key = self._split_path(key)
+        if not self.config.has_section(section):
+            self.config.add_section(section)
+        self.config.set(section, key, value)
+        return self
+
+
+    def get(self, key):
+        """
+        Get a value. It returns ``None`` if it doesn't exist.
+
+        >>> config.get("foo.bar")
+        "42"
+        """
+        section, key = self._split_path(key)
+        if not self.config.has_section(section):
+            return None
+        return self.config.get(section, key)
+
+
+    def items(self):
+        """
+        Yield all items from this config, as tuples of ``(key, value)``
+        """
+        for section in self.config.sections():
+            for k, v in self.config.items(section):
+                yield ('%s.%s' % (section, k), v)
+
+
+    def __del__(self):
+        self.save()
