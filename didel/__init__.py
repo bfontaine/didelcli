ec57a6d0ec7d820f0756a4fc50be9389abcda237
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-13 22:44:31 +0200

    a couple shortcuts added in the main module
    This allows one to write:
    
        from didel import Student
    
    instead of:
    
        from didel.student import Student

--- didel/__init__.py
+++ didel/__init__.py
@@ -1,3 +1,11 @@
 # -*- coding: UTF-8 -*-
 
 __version__ = '0.1.0'
+
+# Shortcuts
+from didel.courses import Course, CourseAssignment, CourseAssignments
+from didel.session import Session
+from didel.student import Student
+
+# silent Pyflakes
+Course, CourseAssignment, CourseAssignments, Session, Student
